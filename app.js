1c2c886ba5d7345ee5c056d07263e27ad0a52c37
leptitube974-ux <leptitube974@gmail.com>
2026-01-09 19:38:10 +0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("PMU PWA chargée");
+
+// Test visuel simple
+document.addEventListener("DOMContentLoaded", () => {
+  const p = document.createElement("p");
+  p.textContent = "App JS OK ✅";
+  p.style.color = "#86efac";
+  document.body.appendChild(p);
+});
